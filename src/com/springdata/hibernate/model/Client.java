a098a67b4d58076a39fc9be437011abd5f806d91
Esraa Khaled <Ekhaled@ntgclarity.com>
2022-07-14 18:12:36 +0200

    add model object

---
+++ src/com/springdata/hibernate/model/Client.java
@@ -0,0 +1,68 @@
+package com.springdata.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="client")
+public class Client {
+	
+	@Id
+	@Column(name="client_id")
+	private Long id;
+	
+	@Column(name="client_user_name")
+	private String userName;
+	
+	@Column(name="client_full_name")
+	private String fullName;
+	
+	@Column(name="client_age")
+	private int age;
+	
+	@Column(name="client_address")
+	private String address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
