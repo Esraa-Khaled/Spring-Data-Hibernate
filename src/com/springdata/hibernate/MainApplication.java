6fb208cc996fa5ccce82b9d9dc57d632fccce1b5
Esraa Khaled <Ekhaled@ntgclarity.com>
2022-07-14 17:58:06 +0200

    add hibernate configuration file

--- src/com/springdata/hibernate/MainApplication.java
+++ src/com/springdata/hibernate/MainApplication.java
@@ -8,18 +8,7 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = "jdbc:postgresql://localhost:5432/employee";
-		String username = "postgres";
-		String password = "hggi";
-		
-		try {
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println("Connected");
-			System.out.println(con);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
 
 }
